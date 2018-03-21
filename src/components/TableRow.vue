474d15a1aadda8fbc9f166cfa655dbab9196ebe0
Paulo dos Santos <paulo.santos@leanix.net>
2018-03-21 15:36:37 +0100

    Initial commit

---
+++ src/components/TableRow.vue
@@ -0,0 +1,32 @@
+<template>
+    <tr
+      @click="$emit('rowClick', row)"
+      v-observe-visibility="(isVisible, entry) => visibilityChangedHandler(isVisible, entry, row)">
+      <table-cell v-for="column in visibleColumns" :row="row" :column="column" :key="column.id"/>
+    </tr>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import TableCell from './TableCell';
+  import VueObserveVisibility from '../directives/observe-visibility/index'
+  Vue.use(VueObserveVisibility)
+
+  export default {
+    props: ['columns', 'row'],
+
+    components: {
+      TableCell,
+    },
+    methods: {
+      visibilityChangedHandler(isVisible, entry, row) {
+        this.$emit('rowVisible', {row: row.data, isVisible})
+      }
+    },
+    computed: {
+      visibleColumns() {
+        return this.columns.filter(column => ! column.hidden);
+      },
+    }
+  }
+</script>
